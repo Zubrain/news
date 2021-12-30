37c9737e4b02b01aa3771763969c6f62a54d72ed
Chinemerem Jideama <ncjideama1@gmail.com>
2021-12-30 19:44:09 +0100

    file upload

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/user/{id}/{name?}', function ($id,$name="Zubby") {
+    return 'Your id is '.$id. " and your name is ".$name;
+})->where('id','[0-9]+');
