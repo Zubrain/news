0b119368133e641c9007745bd71b6a7fd67dbea8
Chinemerem Jideama <ncjideama1@gmail.com>
2021-12-31 11:26:55 +0100

    frontend template setup, css linking, img, js and fonts

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,34 @@
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TechNews - HTML and CSS Template</title>
+
+    <!-- favicon -->
+    <link href="{{ asset('/front/img/favicon.png') }}" rel=icon>
+
+    <!-- web-fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,500' rel='stylesheet' type='text/css'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link href="{{ asset('/front/css/bootstrap.min.css') }}" rel="stylesheet">
+   
+    <!-- font-awesome -->
+    <link href=" {{ asset('/front/fonts/font-awesome/font-awesome.min.css') }}" rel="stylesheet">
+    <!-- Mobile Menu Style -->
+    <link href="{{ asset('/front/css/mobile-menu.css') }}" rel="stylesheet">
+
+    <!-- Owl carousel -->
+    <link href="{{ asset('/front/css/owl.carousel.css') }}" rel="stylesheet">
+    <link href="{{ asset('/front/css/owl.theme.default.min.css') }}" rel="stylesheet">
+    <!-- Theme Style -->
+    <link href="{{ asset('/front/css/style.css') }}" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/user/{id}/{name?}', function ($id,$name="Zubby") {
+//     return 'Your id is '.$id. " and your name is ".$name;
+// })->where('id','[0-9]+');
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
-Route::get('/user/{id}/{name?}', function ($id,$name="Zubby") {
-    return 'Your id is '.$id. " and your name is ".$name;
-})->where('id','[0-9]+');
